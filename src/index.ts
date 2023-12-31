5eaaba2b1d428f06ae5dadcdda6aa2d38046d24d
Samed Ozdemir <samed@xsor.io>
2023-12-30 21:57:36 -0500

    fix(lint): update eslint styling

--- src/index.ts
+++ src/index.ts
@@ -1,167 +1,167 @@
 import axios, { AxiosError, AxiosResponse, InternalAxiosRequestConfig } from 'axios';
 
 export class AwqatSalahApi {
-    private static readonly MILLIS_1_MINUTES: number = 60 * 1000;
+  private static readonly MILLIS_1_MINUTES: number = 60 * 1000;
 
-    private static readonly AUTH_BASE = '/Auth';
-    private static readonly API_BASE = '/api';
-    private static readonly PLACE_ENDPOINT = AwqatSalahApi.API_BASE + '/Place';
+  private static readonly AUTH_BASE = '/Auth';
+  private static readonly API_BASE = '/api';
+  private static readonly PLACE_ENDPOINT = AwqatSalahApi.API_BASE + '/Place';
 
-    private readonly baseUrl: string = 'https://awqatsalah.diyanet.gov.tr';
+  private readonly baseUrl: string = 'https://awqatsalah.diyanet.gov.tr';
 
-    private accessToken = '';
-    private refreshToken = '';
+  private accessToken = '';
+  private refreshToken = '';
 
-    constructor(baseUrl?: string) {
-        if (baseUrl) {
-            this.baseUrl = baseUrl;
-        }
+  constructor(baseUrl?: string) {
+    if (baseUrl) {
+      this.baseUrl = baseUrl;
     }
+  }
 
-    public async login(email: string, password: string) {
-        const response: AxiosResponse<IResult<AuthResponse>, PostDataLogin> = await axios.post(this.baseUrl + `${AwqatSalahApi.AUTH_BASE}/Login`, {
-            email,
-            password,
-        }).catch(this.handleError<AuthResponse, PostDataLogin>)
+  public async login(email: string, password: string) {
+    const response: AxiosResponse<IResult<AuthResponse>, PostDataLogin> = await axios.post(this.baseUrl + `${AwqatSalahApi.AUTH_BASE}/Login`, {
+      email,
+      password,
+    }).catch(this.handleError<AuthResponse, PostDataLogin>)
 
-        if (this.checkSuccess<AuthResponse>(response) && response.data.data) {
-            this.accessToken = response.data.data.accessToken;
-            this.refreshToken = response.data.data.refreshToken;
-            this.queueRefresh();
-        }
+    if (this.checkSuccess<AuthResponse>(response) && response.data.data) {
+      this.accessToken = response.data.data.accessToken;
+      this.refreshToken = response.data.data.refreshToken;
+      this.queueRefresh();
     }
+  }
 
-    private async refreshAccessToken() {
-        console.info('Refreshing access token');
-        const response = await this.get<AuthResponse>(`${AwqatSalahApi.AUTH_BASE}/RefreshToken/${this.refreshToken}`);
+  private async refreshAccessToken() {
+    console.info('Refreshing access token');
+    const response = await this.get<AuthResponse>(`${AwqatSalahApi.AUTH_BASE}/RefreshToken/${this.refreshToken}`);
 
-        if (response) {
-            this.accessToken = response.accessToken;
-            this.refreshToken = response.refreshToken;
-            this.queueRefresh();
-        }
+    if (response) {
+      this.accessToken = response.accessToken;
+      this.refreshToken = response.refreshToken;
+      this.queueRefresh();
     }
+  }
 
-    private queueRefresh(): void {
-        const accessTokenExpiry = this.parseJwt(this.accessToken).exp * 1000;
-        const refreshIn = Math.max(accessTokenExpiry - AwqatSalahApi.MILLIS_1_MINUTES - Date.now(), 0);
-        setTimeout(() => this.refreshAccessToken(), refreshIn);
-        console.info(`Access token will be refreshed in ${refreshIn} millis`);
+  private queueRefresh(): void {
+    const accessTokenExpiry = this.parseJwt(this.accessToken).exp * 1000;
+    const refreshIn = Math.max(accessTokenExpiry - AwqatSalahApi.MILLIS_1_MINUTES - Date.now(), 0);
+    setTimeout(() => this.refreshAccessToken(), refreshIn);
+    console.info(`Access token will be refreshed in ${refreshIn} millis`);
+  }
+
+  public async dailyContent(): Promise<DailyContent | undefined> {
+    return this.get<DailyContent>(`${AwqatSalahApi.API_BASE}/DailyContent`);
+  }
+
+  public async countries(): Promise<Place[] | undefined> {
+    return this.get<Place[]>(`${AwqatSalahApi.PLACE_ENDPOINT}/Countries`);
+  }
+
+  public async states(): Promise<Place[] | undefined> {
+    return this.get<Place[]>(`${AwqatSalahApi.PLACE_ENDPOINT}/States`);
+  }
+
+  public async statesByCountry(countryId: number): Promise<Place[] | undefined> {
+    return this.get(`${AwqatSalahApi.PLACE_ENDPOINT}/States/${countryId}`);
+  }
+
+  public async cities(): Promise<Place[] | undefined> {
+    return this.get<Place[]>(`${AwqatSalahApi.PLACE_ENDPOINT}/Cities`);
+  }
+
+  public async citiesByState(stateId: number): Promise<Place[] | undefined> {
+    return this.get<Place[]>(`${AwqatSalahApi.PLACE_ENDPOINT}/Cities/${stateId}`);
+  }
+
+  public async cityDetail(cityId: number): Promise<CityDetail | undefined> {
+    return this.get<CityDetail>(`${AwqatSalahApi.PLACE_ENDPOINT}/CityDetail/${cityId}`);
+  }
+
+  public async dailyPrayerTime(cityId: number): Promise<PrayerTime[] | undefined> {
+    return this.get<PrayerTime[]>(`${AwqatSalahApi.API_BASE}/PrayerTime/Daily/${cityId}`);
+  }
+
+  public async weeklyPrayerTime(cityId: number): Promise<PrayerTime[] | undefined> {
+    return this.get<PrayerTime[]>(`${AwqatSalahApi.API_BASE}/PrayerTime/Weekly/${cityId}`);
+  }
+
+  public async monthlyPrayerTime(cityId: number): Promise<PrayerTime[] | undefined> {
+    return this.get<PrayerTime[]>(`${AwqatSalahApi.API_BASE}/PrayerTime/Monthly/${cityId}`);
+  }
+
+  public async eidPrayerTime(cityId: number): Promise<EidPrayerTime | undefined> {
+    return this.get<EidPrayerTime>(`${AwqatSalahApi.API_BASE}/PrayerTime/Eid/${cityId}`);
+  }
+
+  public async ramadanPrayerTime(cityId: number): Promise<PrayerTime[] | undefined> {
+    return this.get<PrayerTime[]>(`${AwqatSalahApi.API_BASE}/PrayerTime/Ramadan/${cityId}`);
+  }
+
+  public async dateRange(cityId: number, startDate: Date, endDate: Date): Promise<PrayerTime[] | undefined> {
+    return this.post<PrayerTime[], DateRangeFilter>(`${AwqatSalahApi.API_BASE}/PrayerTime/DateRange/`, {
+      cityId: cityId,
+      startDate: startDate.toISOString(),
+      endDate: endDate.toISOString()
+    });
+  }
+
+  private async get<T>(path: string): Promise<T | undefined> {
+    const response: AxiosResponse<IResult<T>> = await axios.get(this.baseUrl + path, this.getAuthHeaderConfig()).catch(this.handleError<T, void>);
+    return this.extractResponse(response);
+  }
+
+  private async post<T, R>(path: string, data: R): Promise<T | undefined> {
+    const response: AxiosResponse<IResult<T>> = await axios.post(this.baseUrl + path, data, this.getAuthHeaderConfig()).catch(this.handleError<T, void>);
+    return this.extractResponse(response);
+  }
+
+  private getAuthHeaderConfig() {
+    return {
+      headers: {
+        Authorization: `Bearer ${this.accessToken}`
+      }
+    };
+  }
+
+  private extractResponse<T>(response: AxiosResponse<IResult<T>>) {
+    if (this.checkSuccess<T>(response)) {
+      return response.data.data;
+    } else {
+      return undefined;
     }
+  }
 
-    public async dailyContent(): Promise<DailyContent | undefined> {
-        return this.get<DailyContent>(`${AwqatSalahApi.API_BASE}/DailyContent`);
-    }
-
-    public async countries(): Promise<Place[] | undefined> {
-        return this.get<Place[]>(`${AwqatSalahApi.PLACE_ENDPOINT}/Countries`);
-    }
-
-    public async states(): Promise<Place[] | undefined> {
-        return this.get<Place[]>(`${AwqatSalahApi.PLACE_ENDPOINT}/States`);
-    }
-
-    public async statesByCountry(countryId: number): Promise<Place[] | undefined> {
-        return this.get(`${AwqatSalahApi.PLACE_ENDPOINT}/States/${countryId}`);
-    }
-
-    public async cities(): Promise<Place[] | undefined> {
-        return this.get<Place[]>(`${AwqatSalahApi.PLACE_ENDPOINT}/Cities`);
-    }
-
-    public async citiesByState(stateId: number): Promise<Place[] | undefined> {
-        return this.get<Place[]>(`${AwqatSalahApi.PLACE_ENDPOINT}/Cities/${stateId}`);
-    }
-
-    public async cityDetail(cityId: number): Promise<CityDetail | undefined> {
-        return this.get<CityDetail>(`${AwqatSalahApi.PLACE_ENDPOINT}/CityDetail/${cityId}`);
-    }
-
-    public async dailyPrayerTime(cityId: number): Promise<PrayerTime[] | undefined> {
-        return this.get<PrayerTime[]>(`${AwqatSalahApi.API_BASE}/PrayerTime/Daily/${cityId}`);
-    }
-
-    public async weeklyPrayerTime(cityId: number): Promise<PrayerTime[] | undefined> {
-        return this.get<PrayerTime[]>(`${AwqatSalahApi.API_BASE}/PrayerTime/Weekly/${cityId}`);
-    }
-
-    public async monthlyPrayerTime(cityId: number): Promise<PrayerTime[] | undefined> {
-        return this.get<PrayerTime[]>(`${AwqatSalahApi.API_BASE}/PrayerTime/Monthly/${cityId}`);
-    }
-
-    public async eidPrayerTime(cityId: number): Promise<EidPrayerTime | undefined> {
-        return this.get<EidPrayerTime>(`${AwqatSalahApi.API_BASE}/PrayerTime/Eid/${cityId}`);
-    }
-
-    public async ramadanPrayerTime(cityId: number): Promise<PrayerTime[] | undefined> {
-        return this.get<PrayerTime[]>(`${AwqatSalahApi.API_BASE}/PrayerTime/Ramadan/${cityId}`);
-    }
-
-    public async dateRange(cityId: number, startDate: Date, endDate: Date): Promise<PrayerTime[] | undefined> {
-        return this.post<PrayerTime[], DateRangeFilter>(`${AwqatSalahApi.API_BASE}/PrayerTime/DateRange/`, {
-            cityId: cityId,
-            startDate: startDate.toISOString(),
-            endDate: endDate.toISOString()
-        });
-    }
-
-    private async get<T>(path: string): Promise<T | undefined> {
-        const response: AxiosResponse<IResult<T>> = await axios.get(this.baseUrl + path, this.getAuthHeaderConfig()).catch(this.handleError<T, void>);
-        return this.extractResponse(response);
-    }
-
-    private async post<T, R>(path: string, data: R): Promise<T | undefined> {
-        const response: AxiosResponse<IResult<T>> = await axios.post(this.baseUrl + path, data, this.getAuthHeaderConfig()).catch(this.handleError<T, void>);
-        return this.extractResponse(response);
-    }
-
-    private getAuthHeaderConfig() {
-        return {
-            headers: {
-                Authorization: `Bearer ${this.accessToken}`
-            }
-        };
-    }
-
-    private extractResponse<T>(response: AxiosResponse<IResult<T>>) {
-        if (this.checkSuccess<T>(response)) {
-            return response.data.data;
-        } else {
-            return undefined;
-        }
-    }
-
-    private checkSuccess<T>(response: AxiosResponse<IResult<T>>): boolean {
-        return response.status >= 200
+  private checkSuccess<T>(response: AxiosResponse<IResult<T>>): boolean {
+    return response.status >= 200
             && response.status <= 299
             && response.data.success
             && response.data.data !== undefined;
-    }
+  }
 
-    private parseJwt(token: string): JwtToken {
-       return JSON.parse(atob(token.split('.')[1]));
-    }
+  private parseJwt(token: string): JwtToken {
+    return JSON.parse(atob(token.split('.')[1]));
+  }
 
-    private handleError<T, D>(e: AxiosError<IResult<T>, D>): AxiosResponse<IResult<T>, D> {
-        const response = e.response || {
-          data: {
-            success: false,
-            message: 'Unknown error'
-          },
-          status: 0,
-          statusText: '0',
-          headers: {},
-          config: e.config as InternalAxiosRequestConfig<D> || {
-            method: 'unknown',
-            url: 'unknown'
-          },
-          message: e.message
-        };
+  private handleError<T, D>(e: AxiosError<IResult<T>, D>): AxiosResponse<IResult<T>, D> {
+    const response = e.response || {
+      data: {
+        success: false,
+        message: 'Unknown error'
+      },
+      status: 0,
+      statusText: '0',
+      headers: {},
+      config: e.config as InternalAxiosRequestConfig<D> || {
+        method: 'unknown',
+        url: 'unknown'
+      },
+      message: e.message
+    };
 
-        console.error(`Error during API call, url: ${response.config.url}, method: ${response.config.method}, status: ${response.status}, axiosErrorMessage: ${e.message}, apiSuccess: ${response.data.success}, apiErrorMessage: ${response.data.message}`);
+    console.error(`Error during API call, url: ${response.config.url}, method: ${response.config.method}, status: ${response.status}, axiosErrorMessage: ${e.message}, apiSuccess: ${response.data.success}, apiErrorMessage: ${response.data.message}`);
 
-        return response;
-    }
+    return response;
+  }
 }
 
 type IResult<T = never> = {
